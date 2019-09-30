d17854939a52267d4cee5497d2e9437d7100b42d
SivalingamM <murugan.sivalingam@rabobank.nl>
2019-09-30 13:47:21 +0530

    Cros enable

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,10 +1,12 @@
 package io.pivotal.pal.tracker;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class WelcomeController {
 
     private final String WELCOME_MESSAGE;
